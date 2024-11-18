fd9083b7974e81bcdb39174c9a849712bc7d031c
Kazuma tsutsumi <qianzhendi5@gmail.com>
2024-11-18 21:49:01 +0900

    report1

--- app5.js
+++ app5.js
@@ -19,10 +19,15 @@ app.get("/icon", (req, res) => {
 });
 
 app.get("/luck", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor( Math.random() * 7 + 1 );
   let luck = '';
   if( num==1 ) luck = '大吉';
   else if( num==2 ) luck = '中吉';
+  else if( num==3)luck ='吉';
+  else if( num==4)luck ='小吉';
+  else if( num==5)luck ='末吉';
+  else if( num==6)luck ='凶';
+  else if( num==7)luck ='大凶';
   console.log( 'あなたの運勢は' + luck + 'です' );
   res.render( 'luck', {number:num, luck:luck} );
 });
@@ -82,4 +87,56 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
+
+app.get('/calculate', (req, res) => {
+  const number1 = parseFloat(req.query.number1);
+  const number2 = parseFloat(req.query.number2);
+  const operator = req.query.operator;
+
+  let result;
+  if (operator === '+') {
+      result = number1 + number2;
+  } else if (operator === '-') {
+      result = number1 - number2;
+  } else if (operator === '*') {
+      result = number1 * number2;
+  } else if (operator === '/') {
+      result = number2 !== 0 ? number1 / number2 : 'エラー: 0で割ることはできません';
+  } else {
+      result = '無効な演算子です';
+  }
+  
+
+  res.render('result', { number1, number2, operator, result });
+});
+
+app.get("/kazu", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number( req.query.win )|| 0;
+  let total = Number( req.query.total )|| 0;
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 10 + 1 );
+  let judgement = '';
+  let cpu = num;
+  if (cpu == hand) {
+    judgement = '一致した！';
+    win += 1;
+  }
+  else{
+    judgement = '一致しなかった！';
+  }
+
+  total += 1;
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'kazu', display );
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
+
+
